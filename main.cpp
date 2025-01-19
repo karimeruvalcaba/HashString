2ff30c1aef0520ede4aa3ca613649a6f8d910be0
Karime Itzel Ruvalcaba Pérez <137795975+karimeruvalcaba@users.noreply.github.com>
2025-01-19 19:59:44 +0000

    terminado

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 /* HEADER
  * Este programa calcula el valor hash de una cadena usando un algoritmo
- * sencillo. Complejidad computacional:
+ * sencillo. 
+ * Complejidad computacional:
  * 1. Recorrer la cadena: O(n) - Se itera a través de todos los caracteres de la
  * cadena.
- * 2. Operaciones de hash: O(1) por carácter - Se realizan multiplicaciones y
+ * 2. Operaciones del hash: O(1) por carácter - Se realizan multiplicaciones y
  * sumas por cada carácter. Total: O(n) - La complejidad depende de la longitud
  * de la cadena de entrada.
  */
@@ -13,28 +14,37 @@
  * Sergio Alfonso Casillas Santoyo A01424863
  * Karime Itzel Ruvalcaba Pérez A01656188
  */
-
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-unsigned int hashString(const string &str) {
-  unsigned int hash = 7;
-  for (char c : str) {
-    hash = hash * 31 + c;
-  }
-  return hash;
+/*Funcion para hashear un string
+Complejidad computacional:
+- Calcular la longitud de la cadena: O(1)
+- Iterar sobre la cadena: O(n), donde n es la longitud de la cadena.
+- Operaciones por carácter (multiplicación, suma, modulo): O(1) por iteración.
+Total: O(n)
+*/ 
+long long prhf(string &s) {
+    int n = s.length();
+    int p = 31;
+    int m = 1e9 + 9;
+    long long valorHash = 0;
+    long long potencia = 1;
+    for (int i = 0; i < n; i++){
+      valorHash = (valorHash + (s[i]- 'a' + 1)* potencia) % m;
+      potencia = (potencia * p) % m;
+    }
+    return valorHash;
 }
 
 int main() {
-  string input;
+    string s;
+    cout << "Ingresa el input para hacer el hash string: ";
+    getline(cin, s);
 
-  cout << "Ingrese una cadena: ";
-  getline(cin, input);
+    cout << "Salida: " << prhf(s) << endl;
 
-  unsigned int hashValue = hashString(input);
-
-  cout << "\nEl valor hash es: " << hashValue << endl;
-
-  return 0;
+    return 0;
 }
